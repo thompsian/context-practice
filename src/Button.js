f92c03660f165599135ed8390a7f863db4680932
thompsian <thompsian@gmail.com>
2021-10-26 14:49:15 -0700

    changing context

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,11 @@
 import React from "react"
-import PropTypes from "prop-types"
 import {ThemeContextConsumer} from "./themeContext"
 
 function Button(props) {
     return (
         <ThemeContextConsumer>
-            {theme => (
-                <button className={`${theme}-theme`}>Switch Theme</button>
+            {context => (
+                <button onClick={context.toggleTheme} className={`${context.theme}-theme`}>Switch Theme</button>
             )}
         </ThemeContextConsumer>
     )    
